79c1f090f6096225156f23019c8428324b3d9eb7
Misha Lvovsky <lvovsky.m@northeastern.edu>
2025-02-26 17:58:12 +0000

    got rid of an accidental self that was breaking things

--- lerobot/common/datasets/lerobot_dataset.py
+++ lerobot/common/datasets/lerobot_dataset.py
@@ -810,7 +810,7 @@ class LeRobotDataset(torch.utils.data.Dataset):
 
             if self.features[key]["dtype"] in ["image", "video"]:
                 img_path = self._get_image_file_path(
-                    episode_index=self.episode_buffer["episode_index"], image_key=key, frame_index=frame_index
+                    episode_index=episode_buffer["episode_index"], image_key=key, frame_index=frame_index
                 )
                 if frame_index == 0:
                     img_path.parent.mkdir(parents=True, exist_ok=True)
